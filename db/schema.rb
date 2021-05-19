be1c53506ce48c855ecfca8f3ef44cd25b0e2fb2
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-05-19 17:55:48 +0100

    Applied basic styling to the sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_140604) do
-
-# ActiveRecord::Schema.define(version: 2021_05_18_145235) do downstream version TO DELETE?
-
+ActiveRecord::Schema.define(version: 2021_05_18_145235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
