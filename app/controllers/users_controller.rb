ef5f613926a443fe160fbfae5e5cd2d638f70748
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-19 14:43:35 +0100

    check db notes added - unit and feature test will fail until method complete (obviously)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,23 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :dob, :email, :password)
   end
+
+  def check_db
+    # if User.exists?(email: params[:user][:email]) == false
+    #   redirect_to "/", notice: "Success: New account created"
+    # else
+    #   notice: "Error: This email is already in use"
+    # use rails exist method to check if user already in database and prevent multiple account creation - raise flash error message,
+    # end
+  end
+
+  
+  def notice
+
+  end
+
+  # def alert
+
+  # end
+
 end
