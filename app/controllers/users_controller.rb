65dddb48c7c5aa5921ccfbaa280044487db91a43
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-21 12:15:40 +0100

    sign up works againgit add .git add .git add .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,60 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    # Nav bar will need user profile link in this html
+  end
+
   def new
     @user = User.new
     @errors = flash[:errors]
   end
 
   def create
-   @user = User.create(user_params)
-    if @user.errors.present?
-      flash[:errors] = "Error: This email is already in use"
-      redirect_to "/users/new"
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Success: New Account Created"
+      redirect_to "/"
     else
-      session[:user_id]=@user.id
-      flash[:notice] = "Success: New Account Created"
-      redirect_to root_path
+      render 'new'
     end
 
-  end
 
+  #  @user = User.create(user_params)
+  #   if @user.errors.present?
+  #     flash[:errors] = "Error: This email is already in use"
+  #     redirect_to "/users/new"
+  #   else
+  #     session[:user_id]=@user.id
+  #     flash[:notice] = "Success: New Account Created"
+  #     redirect_to root_path
+  #   end
+
+  end
+  
   private
   
-  def user_params
-    params.require(:user).permit(:name, :dob, :email, :password, :password_confirmation)
-  end
-
-  def check_db
-    if User.exists?(email: params[:user][:email])
-      true
-    else
-      false
+    def user_params
+      params.require(:user).permit(:name, :dob, :email, :password, :password_confirmation)
     end
-    # can refactor code to remove conditional (implicit return - keep only User.exists?(email: params[:user][:email]))
-  
-  end
 
-  
-  def notice
+    def check_db
+      if User.exists?(email: params[:user][:email])
+        true
+      else
+        false
+      end
+      # can refactor code to remove conditional (implicit return - keep only User.exists?(email: params[:user][:email]))
+    
+    end
 
-  end
+    
+    def notice
 
-  # def alert
+    end
 
-  # end
+    # def alert
+
+    # end
 
 end
