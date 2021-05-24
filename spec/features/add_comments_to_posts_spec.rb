ff900486ab489f420c55468016d6581492c5b1a2
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-05-24 14:24:20 +0100

    Updated the feature test for adding comments

--- spec/features/add_comments_to_posts_spec.rb
+++ spec/features/add_comments_to_posts_spec.rb
@@ -6,8 +6,9 @@ RSpec.feature "anonymous can comment on the post", type: :feature do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    fill_in "Comment", with: "Hello, person"
-    click_button "Add comment"
+    fill_in "Name", with: "Joe"
+    fill_in "Body", with: "Hello, person"
+    click_button "Create Comment"
     expect(page).to have_content("Hello, person")
   end
 end
