5f035e8482c96a908c8a50569d0700299af82f5d
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-25 10:50:46 +0100

    rspec test all pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_112907) do
-
-# merged from yesterday( 24/05/21) ActiveRecord::Schema.define(version: 2021_05_24_160811) do
-
+ActiveRecord::Schema.define(version: 2021_05_24_160811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,15 +43,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_112907) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
-  create_table "images", force: :cascade do |t|
-    t.binary "image"
-    t.bigint "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_images_on_post_id"
-  end
-  
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "body"
@@ -62,6 +50,14 @@ ActiveRecord::Schema.define(version: 2021_05_24_112907) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.binary "image"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_images_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -86,7 +82,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_112907) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "images", "posts"
   add_foreign_key "comments", "posts"
+  add_foreign_key "images", "posts"
   add_foreign_key "posts", "users"
 end
--- spec/features/add_comments_to_posts_spec.rb
+++ spec/features/add_comments_to_posts_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.feature "anonymous can comment on the post", type: :feature do
   scenario "comments appear under the posts" do
+    visit "/login"
+    click_button "Sign Up"
+    fill_in "Name", with: "John Doe"
+    fill_in "Email", with: "jdoe@email.com"
+    fill_in "Dob", with: "1992-01-01" 
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Create Account"
+    #---------------------
+
+    fill_in "Email", with: "jdoe@email.com"
+    fill_in "Password", with: "password"
+    click_button "Login"
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
