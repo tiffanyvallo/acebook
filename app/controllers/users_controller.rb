efc153cc093c7c6099d707edfb1dbf2e7c462b6b
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-25 12:38:27 +0100

    added profile edit form, still not updating to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,15 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
-    redirect_to current_user
+    show
+    if current_user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to current_user
+    else
+      render 'edit'
+    end
+    # current_user.update(user_params)
+    # redirect_to current_user
   end
   
   private
