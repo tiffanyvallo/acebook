a74a9158d23b726008816307772ca6eb2128b046
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-27 00:28:17 +0100

    Profile posts fixed: now showing ONLY posts for the owner of each profile - BUG: posts are nesting inside one another as you add them. No idea why. WTH.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = current_user.posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc)
     # Nav bar will need user profile link in this html
   end
 
