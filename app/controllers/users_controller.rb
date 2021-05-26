2b5b704c767a8f1d86daf8d2b4e95cb0ce3890ec
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-26 23:52:04 +0100

    Users posts appear on their profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = current_user.posts.order(created_at: :desc)
     # Nav bar will need user profile link in this html
   end
 
@@ -31,9 +32,9 @@ class UsersController < ApplicationController
     end
   end
 
-  # def user_posts
-  #   @posts = current_user.posts.order(created_at: :desc)
-  # end
+  def user_posts
+    @posts = current_user.posts.order(created_at: :desc)
+  end
   
 
   private
