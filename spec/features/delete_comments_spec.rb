e0b1fdb98f18dd0881b8391bb294915499b5969e
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-28 10:31:11 +0100

    delte for user only

--- spec/features/delete_comments_spec.rb
+++ spec/features/delete_comments_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature "Delete comment", type: :feature do
     # fill_in "Name", with: "Joe"
     fill_in "comment[body]", with: "Hello, person"
     click_button "Post"
-    click_button "Delete Comment"
+    click_button "❌"
     expect(page).not_to have_content("Hello, person")
   end
 end
